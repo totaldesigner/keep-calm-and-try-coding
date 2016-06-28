b8d8d110983c311dbd1c139a6b83ca4f0c286eb2
totaldesigner <mspark@humaxdigital.com>
2016-06-29 02:05:25 +0900

    Detecting if a string has unique characters

---
+++ javascript.js
@@ -0,0 +1,13 @@
+/**
+ * Created by mspark on 16. 6. 29.
+ */
+
+// Detecting if a string has unique characters
+var word = "Unique";
+var reg = /(.)(.*?)(\1)/g;
+console.log(reg.test(word) ? 'Duplicated' : 'Unique');
+
+var unique = word.split('').filter(function(element,index,array){
+    return array.indexOf(element) == index;
+}).join('');
+console.log(unique.length == word.length ? "Unique": 'Duplicated');
